29e315a31a2ec7c5b3fbf45dc4413b234536e718
Pamparau Robert <robertpamparau0@gmail.com>
2022-03-02 23:24:45 +0200

    Configured db credentials

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  db: process.env.DB || 'postgres',
+  dialect: process.env.DB_DIALECT || 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
