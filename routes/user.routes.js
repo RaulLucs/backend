8c2538e92331d7f9436dd0b698f0832e735c3833
Bob <raullucules@gmail.com>
2022-03-11 11:52:04 +0200

    nuth

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const auth = require('../middleware/auth');
 
 module.exports = (app) => {
-  router.post('/', auth, users.create);
-  router.get('/', auth, users.findAll);
+  router.post('/', users.create);
+  router.get('/', users.findAll);
   router.get('/:id', auth, users.findOne);
   router.put('/:id', auth, users.update);
   router.post('/login', users.login);
