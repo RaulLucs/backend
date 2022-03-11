0ac00391b6ec74acf67701639e85e22c69fb0f02
Bob <raullucules@gmail.com>
2022-03-11 18:40:18 +0200

    d

--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -1,5 +1,8 @@
 const dbConfig = require('./db.config');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(dbConfig.connectionString);
+const sequelize = new Sequelize(dbConfig.connectionString,{
+  ssl: {
+  rejectUnauthorized: false
+}});
 module.exports = { sequelize };
