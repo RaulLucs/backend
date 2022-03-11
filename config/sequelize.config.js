f6c51e6835d037bdcc6f03347a79307f2f483e7d
Bob <raullucules@gmail.com>
2022-03-11 17:00:26 +0200

    d

--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -1,24 +1,5 @@
 const dbConfig = require('./db.config');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
-  host: dbConfig.host,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-  logging: false,
-  ssl: true,
-  connectionString: process.env.DATABASE_URL,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized:false,
-    }
-  },
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
+const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, process.env.DATABASE_URL);
 module.exports = { sequelize };
