373cdfcd221add466b879ac03dd83a78a0eb56fb
Bob <raullucules@gmail.com>
2022-03-11 15:35:42 +0200

    d

--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
   host: dbConfig.host,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
+  ssl: {rejectUnauthorized: false},
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
