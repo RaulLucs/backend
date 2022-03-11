701ad4f09239e011b00c5235256bc66e193dc9d7
Bob <raullucules@gmail.com>
2022-03-11 13:39:39 +0200

    D

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ sequelize.sync();
 userRoutes(app);
 bulidingRoutes(app);
 officeRoutes(app);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5432;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
