79247a8ddb65392805ba919ff55e068aac37a4c6
Bob <raullucules@gmail.com>
2022-03-11 15:33:00 +0200

    d

--- config/db.config.js
+++ config/db.config.js
@@ -4,8 +4,7 @@ module.exports = {
   password: process.env.DB_PASSWORD || 'postgres',
   db: process.env.DB || 'postgres',
   dialect: process.env.DB_DIALECT || 'postgres',
-  connectionString: process.env.DATABASE_URL,
-  ssl: {rejectUnauthorized: false},
+  ssl: {rejectUnauthorized: false}
   pool: {
     max: 5,
     min: 0,
