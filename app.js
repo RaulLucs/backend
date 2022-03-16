3fdbec80c5a5b55461885c7552d98fcdfc2cbab3
Bob <raullucules@gmail.com>
2022-03-17 00:41:45 +0200

    Changed sequelize.sync to force:true

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const { sequelize } = require('./config/sequelize.config');
-const userRoutes = require('./routes/user.routes');
-const buildingRoutes = require('./routes/building.routes');
-const officeRoutes = require('./routes/office.routes');
-const remoteRoutes = require('./routes/remote.routes');
-const express = require('express');
-const cors = require('cors');
+const { sequelize } = require("./config/sequelize.config");
+const userRoutes = require("./routes/user.routes");
+const buildingRoutes = require("./routes/building.routes");
+const officeRoutes = require("./routes/office.routes");
+const remoteRoutes = require("./routes/remote.routes");
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: "http://localhost:3000",
 };
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   //when we get an http get request to the root/homepage
-  res.send('Hello, Assist!');
+  res.send("Hello, Assist!");
 });
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// sequelize.sync({ force: true });
-sequelize.sync();
+sequelize.sync({ force: true });
+//sequelize.sync();
 userRoutes(app);
 buildingRoutes(app);
 officeRoutes(app);
